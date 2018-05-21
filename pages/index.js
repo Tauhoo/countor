eeb08719aebdea271ab2132a6c82ad206b9f5113
wachirawit wacharak <tauhoo_ice@hotmail.com>
2018-05-21 15:11:07 +0700

    set up

---
+++ pages/index.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import Head from 'next/head'
+export default class extends Component {
+  render() {
+    return (
+      <div>
+        <Head>
+          <title>Countor</title>
+          <meta name="viewport" content="initial-scale=1.0, width=device-width" key="viewport" />
+        </Head>
+        <body>
+          <h1>ICE</h1>
+        </body>
+      </div>
+    )
+  }
+}
